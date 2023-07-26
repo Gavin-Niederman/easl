941eb1ae4445b8d7261a163c81b5f98304edfff8
Gavin-Niederman <gavinniederman@gmail.com>
2023-07-26 00:16:17 -0700

    switch to pest

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,5 @@
 #![feature(iter_next_chunk)]
 
 pub mod parse;
-pub mod scan;
 
-use std::path::PathBuf;
-
-use clap::Parser;
 pub use parse::*;
-pub use scan::*;
-
-#[derive(Parser, Clone)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    source_file: PathBuf,
-}
-
-lazy_static::lazy_static! {
-    pub static ref ARGS: Args = Args::parse();
-    pub static ref SOURCE: &'static str = {
-        let Ok(source) = std::fs::read_to_string(ARGS.source_file.clone()) else {
-            panic!("{}", miette::miette!("Could not read source file"))
-        };
-
-        Box::leak(source.into_boxed_str())
-    };
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use miette::Result;
+#[derive(Parser, Clone)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+source_file: PathBuf,
+}
 
 fn main() -> Result<()> {
-    let tokens = easl::scan()?;
+    let args = Args::parse();
+    let Ok(source) = std::fs::read_to_string(args.source_file.clone()) else {
+        panic!("{}", miette::miette!("Could not read source file"))
+    };
 
-    println!(
-        "{:?}",
-        tokens
-            .clone()
-            .map(|t| t.token_type)
-            .collect::<Vec<easl::TokenType>>()
-    );
+   let ast = easl::parse(&source).unwrap();
 
-    let expr = easl::parse(tokens)?;
-
-    println!("{:?}", expr);
+    println!("{:#?}", ast);
 
     Ok(())
 }
